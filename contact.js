17c79d5eaaf8cfed28a3d917eea6349b770d31d8
Soumyaneel Ghosh <Ghoshsoumyaneel@gmail.com>
2024-10-03 23:11:16 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,20 +1,16 @@
 
 function handleSubmit(event) {
     const form = document.getElementById('EnqueryForm');
-
     if (!form.checkValidity()) {
         form.classList.add('was-validated'); 
         event.preventDefault(); 
         return false;
     }
-
     event.preventDefault();
-
     const name = document.getElementById('Name').value;
     const email = document.getElementById('Email').value;
     const phone = document.getElementById('phone').value;
     const message = document.getElementById('Message').value;
-
     const userData = `
         <b>Name:</b> ${name} <br>
         <b>Email:</b> ${email} <br>
@@ -22,10 +18,8 @@ function handleSubmit(event) {
         <b>Message:</b> ${message}
     `;
     document.getElementById('userData').innerHTML = userData;
-
     const confirmationModal = new bootstrap.Modal(document.getElementById('confirmationModal'), {});
     confirmationModal.show();
-
     document.getElementById('okButton').addEventListener('click', function() {
         window.location.href = "index.html"; 
     });
